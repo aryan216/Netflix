00946b6ae29926acd436dd41a7cccea9fa1135f1
aryan216 <aniketani603@gmail.com>
2023-09-17 22:58:32 +0530

    signin/signup

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,26 +3,8 @@ import "./banner.css";
 import logo from "../assets/logo.png";
 const Header = () => {
   return (
-    <div className="">
-      <div className=" w-full banner h-screen  flex bg-red bg-red-500 justify-between  ">
-        <div className="flex w-full justify-between  ">
-          <div>
-            <img className="w-64" src={logo} alt="netflix-logo" />
-          </div>
-          <div className="flex ">
-            <div className="w-52 mr-10 mt-5">
-              <select
-                id="countries"
-                className="bg-transparent border border-gray-300 text-black/70 text-sm rounded-md block w-full p-2.5"
-              >
-                <option selected>English</option>
-                <option value="US">Hindi</option>
-              </select>
-            </div>
-           
-          </div>
-        </div>
-      </div>
+    <div className="absolute px-8 py-2 w-full z-10 bg-gradient-to-b from-black">
+      <img className="w-44 " src={logo} alt="" />
     </div>
   );
 };
--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,18 +1,29 @@
 import React from "react";
 import Header from "./Header";
-
+import banner from "../assets/banner.jpg" 
+import { useState } from "react";
 const Login = () => {
+  const [isSignInForm,setIsSignInForm]=useState(true);
+  const toggleSignInForm=()=>{
+    setIsSignInForm(!isSignInForm);
+  }
   return (
     <div>
       <Header />
-      <div className="absolute top-1/2  right-1/3   ">
-        <div className="flex items-center">
-          <input placeholder="Search for movies " className=" px-3 w-80 py-4" />
-          <button className="px-10 py-4 bg-red-700 text-white">Get Started</button>
-        </div>
+      <div className="absolute">
+        <img src={banner} alt="" />
       </div>
+      <form className="absolute p-12 bg-black/75 w-3/12 my-36 mx-auto right-0 left-0 text-white rounded-lg ">
+        <h1 className="font-bold p-4 text-3xl">{isSignInForm?"Sign In":"Sign Up"}</h1>
+
+        {!isSignInForm &&(<input type="text" placeholder="Full Name" className="p-4 m-2 w-full rounded-lg bg-neutral-700"/>)}
+        <input type="text" placeholder="Email" className="p-4 m-2 w-full rounded-lg bg-neutral-700"/>
+        <input type="password" placeholder="Password" className="p-4 m-2 w-full rounded-lg bg-neutral-700" />
+        <button className="p-4 m-2 w-full bg-red-600 rounded-lg ">{isSignInForm?"Sign In":"Sign Up"}</button>
+        <p className="p-4 m-2" onClick={toggleSignInForm}>{isSignInForm? "New to Netflix? Sign Up Now" : "Already Registered?  Sign In"}</p>
+      </form>
     </div>
   );
 };
-
+ 
 export default Login;
